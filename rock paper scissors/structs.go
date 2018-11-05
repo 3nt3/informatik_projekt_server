116e027e85a8bdcf28ae63e77ed1444a7ab8dd83
Niels Schlegel <niels.dingsbums@gmx.de>
2018-11-05 20:32:33 +0100

    Add rock paper scissors game-mode

---
+++ rock paper scissors/structs.go
@@ -0,0 +1,14 @@
+package rock_paper_scissors
+
+// Rooms are used as areas for two players to play in
+type room struct {
+	id      int
+	players []player
+}
+
+// The player type
+type player struct {
+	id    int
+	name  string
+	score int
+}
